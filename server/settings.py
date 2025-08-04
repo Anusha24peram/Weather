08d98e0f7279f86f796d95afdc38353f0b10ff02
Anusha Peram <161455785+Anusha24peram@users.noreply.github.com>
2025-08-04 20:14:21 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c$m)_r@cxo5_k1%q%zx2un50u0qp%70&!9gjy#*245lc#_$opk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
